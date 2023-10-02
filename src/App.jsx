a72bd7376e829e58512c8cc31fca1c1e7f0648c2
GarimaUttam <garimauttam191203@gmail.com>
2023-10-02 17:56:20 +0530

    added the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import { Navbar, Hero, Footer } from './components';
+import React from 'react';
+import { Navbar, Hero, Footer, Memory, Explore, Advertise, Pricings, Banner, Newslatter } from './components';
+import { hero, navlinks, memory, placesAPI, brands, pricingapi, bannerAPI, footerAPI } from './data/travigodata';
 
 const App = () => {
   return (
-     <>
-     <Navbar/>
-     <Hero/>
-     <Footer/>
-    </>
-  )
-}
+   <>
+      <Navbar navlinks={navlinks} />
+        <Hero hero={hero} />
+        <Memory memory={memory} />
+        <Explore title="Explore The Beauty of World" placesAPI={placesAPI} />
+        <Advertise brands={brands} />
+        <Pricings pricingapi={pricingapi} />
+        <Banner bannerAPI={bannerAPI} />
+        <Newslatter />
+      <Footer footerAPI={footerAPI} />
+   </>
+  );
+};
 
-export default App
+export default App;
