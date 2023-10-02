4e05f0c40d1ff0a6737e825f97bef62d3e59c704
GarimaUttam <garimauttam191203@gmail.com>
2023-10-03 05:10:26 +0530

    changed company section

--- src/components/Advertise.jsx
+++ src/components/Advertise.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Advertise = ({ brands }) => {
   return (
    <>
-      <div className='my-16 lg:my-7 w-9/12 lg:w-[85vw] m-auto flex items-center justify-between xl:gap-7 overflow-x-scroll scroll-smooth scroll-hidden'>
+      <div className='py-16 lg:py-7  lg:w-[100vw] m-auto flex items-center justify-between xl:gap-7 overflow-x-scroll scroll-smooth scroll-hidden bg-[#657487] mx-0 space-between px-[90px]'>
         {brands?.map((val, i) => (
             <img key={i} src={val.iconSrc} alt="brands/icon" className="w-44 h-auto object-fill xl:w-38 lg:w-32 hover:scale-105 transition-all duration-300 cursor-pointer filter drop-shadow-md" />
         ))}
