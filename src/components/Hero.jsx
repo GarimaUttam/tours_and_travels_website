393fc26fbd392446d521be6e4f21c0138df93b57
GarimaUttam <garimauttam191203@gmail.com>
2023-10-03 05:08:18 +0530

    change bg color of memory

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-const Hero = ({ hero: { title, subtitle, btn1, btn2, text, img } }) => {
+const Hero = ({ hero: { title, btn1, btn2, text, img } }) => {
+  const playerRef = useRef(true);
   return (
    <>
-      <div className='flex flex-col bg-gradient-to-b from-emerald-200 to-white h-auto w-auto'>
+      <div className='flex flex-col bg-gradient-to-b from-[rgb(0,0,0)] to-[#657487] h-auto w-auto'>
         <div className='travigo-container grid items-start justify-items-center'>
-
           <div className='grid items-center justify-items-center mt-36 mb-16 md:mt-28 md:mb-12'>
-            <h1 className='text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl xsm:text-3xl text-black filter drop-shadow-lg'>{title}</h1>
-            <h1 className='text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl xsm:text-3xl text-black filter drop-shadow-lg'>{subtitle}</h1>
-            <p className='text-base my-5 text-center sm:text-sm'>{text}</p>
+            <h1 className='text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl xsm:text-3xl text-[#d6d3d1] filter drop-shadow-lg'>{title}</h1>
+            <h1 className='text-7xl flex font-bold lg:text-6xl md:text-5xl sm:text-4xl xsm:text-3xl text-[#4fadd8] filter drop-shadow-lg'>Then Go <h1 className='ml-3 text-7xl font-bold lg:text-6xl md:text-5xl sm:text-4xl xsm:text-3xl text-[#d6d3d1] filter drop-shadow-lg'>Explore</h1>
+            </h1>
+            <p className='text-base text-[#cffafe] my-5 text-center sm:text-sm'>{text}</p>
             <div className='flex items-center justify-center gap-11 sm:gap-3 sm:flex-col sm:w-full'>
               <button type='button' className='button-emrald'>{btn1}</button>
               <button type='button' className='button-light'>{btn2}</button>
             </div>
           </div>
           <div className='flex items-center justify-center mb-11'>
-
-
             
-            <img src={img} alt='dashbaord/img' className='h-[85vh] w-full object-fill lg:h-[75vh] md:h-[57vh] sm:h-[35vh] sm:object-contain drop-shadow-emrald' />
+            <img src={img} alt='dashbaord/img' className='h-[85vh] w-[900px] object-fill lg:h-[75vh] md:h-[57vh] sm:h-[35vh] sm:object-contain drop-shadow-emrald rounded-[3rem]'/>
+            {/* <ReactPlayer url={video}  className='h-[85vh] w-full object-fill lg:h-[75vh] md:h-[57vh] sm:h-[35vh] sm:object-contain drop-shadow-emrald'/> */}
           </div>
         </div>
       </div>
--- src/components/Memory.jsx
+++ src/components/Memory.jsx
@@ -4,18 +4,18 @@ const Memory = ({ memory: { title, text, subtitle, img, experience } }) => {
     // console.log(memory)
   return (
    <>
-      <div className='relative my-16 md:mb-7'>
+      <div className='relative md:mb-7 bg-gradient-to-b from-[#657487] to-[rgb(0,0,0)]'>
         <div className='travigo-container flex items-center justify-between gap-16 xl:gap-9 lg:flex-col-reverse'>
             <div className='flex items-center justify-start lg:justify-center w-full max-w-md'>
-                <img src={img} alt="memory/img" className='w-auto h-[55vh] object-fill' />
+                <img src={img} alt="memory/img" className='w-auto h-[55vh] object-fill rounded-tr-[15rem] rounded-bl-[15rem] pb-5' />
             </div>
             <div className='grid items-center w-full max-w-2xl lg:text-center'>
-                <h1 className='text-5xl lg:text-4xl md:text-3xl font-bold text-slate-900 filter drop-shadow-lg'>{title}</h1>
-                <h1 className='text-5xl lg:text-4xl md:text-3xl font-bold text-slate-900 filter drop-shadow-lg'>{subtitle}</h1>
-                <p className='text-base my-5 sm:text-sm'>{text}</p>
+                <h1 className='text-5xl lg:text-4xl md:text-3xl font-bold text-[#d6d3d1] filter drop-shadow-lg'>{title}</h1>
+                <h1 className='text-5xl lg:text-4xl md:text-3xl font-bold text-[#89d5ef] filter drop-shadow-lg'>{subtitle}</h1>
+                <p className='text-base my-5 sm:text-sm text-[#d6d3d1]'>{text}</p>
                 <div className='grid items-center grid-cols-3 gap-7 xl:gap-3'>
                     {experience?.map((val, i) => (
-                        <div key={i} className="bg-gradient-to-b from-emerald-300 to-green-300 shadow-lg shadow-emerald-200 flex items-center justify-center flex-col py-7 px-5 xl:p-5 rounded-lg text-slate-900 filter cursor-pointer hover:scale-105 transition-all duration-400">
+                        <div key={i} className="bg-gradient-to-b from-[#22d3ee] to-[#3b82f6] shadow-lg shadow-[#4f668a] flex items-center justify-center flex-col py-7 px-5 xl:p-5 rounded-lg text-[#d6d3d1] filter cursor-pointer hover:scale-105 transition-all duration-400">
                             <h1 className='text-3xl xl:text-2xl sm:text-xl font-bold drop-shadow-lg'>{val.number}</h1>
                             <p className='text-lg xl:text-base sm:text-sm xsm:text-xs font-medium'>{val.title}</p>
                         </div>
