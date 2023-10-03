b61770e6ea11d8c31cb6f7f560f65351072feef8
GarimaUttam <garimauttam191203@gmail.com>
2023-10-03 17:46:18 +0530

    added the banner name

--- src/data/travigodata.js
+++ src/data/travigodata.js
@@ -123,7 +123,7 @@ export const pricingapi = {
 
 export const bannerAPI = {
   "title": "The Greet Outdoors",
-  "text": "Whislist Curated By Travigo.",
+  "text": "Whislist Curated By Travelia.",
   "imgSrc": banner,
   "btnText": "Explore More"
 }
